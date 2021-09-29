74021a1e77de34b46c72ee0d7064ce2a6660b235
andressawerner <awkpereira@inf.ufpel.edu.br>
2021-09-29 12:41:59 -0300

    fix/algoritmo exato funcionando

--- assistants/combinations.js
+++ assistants/combinations.js
@@ -1,15 +1,16 @@
 function combinations(qtdNodos, factorial) {
   const combinations = []
+  const combinationsNot = []
   const maxCombinations = factorial
 
   const envolved = []
-  for (let i = 0; i < qtdNodos; i++) {
+  for (let i = 1; i < qtdNodos; i++) {
     envolved.push(i)
   }
 
   let i = 0
   while (i != maxCombinations) {
-    let combination = ''
+    let combination = ['0']
     let disponiveis = envolved.slice()
 
     for (let j = 0; j < envolved.length; j++) {
@@ -23,17 +24,26 @@ function combinations(qtdNodos, factorial) {
       const nodo = disponiveis.splice(proxNodo, 1)
       //console.log(nodo)
 
-      combination += nodo + ' '
+      combination.push(nodo)
     }
-    combination = combination.slice(0, combination.length - 1)
+    //combination = combination.slice(0, combination.length - 1)
     //console.log(combination)
-    if (!combinations.includes(combination)) {
-      console.log(`${i} combination`)
-      combinations.push(combination)
-      i++
+    if (!combinationsNot.includes(combination.join(' '))) {
+      //console.log(`${i} combination`)
+      combinations.push(combination.join(' '))
+      combinationsNot.push(combination.join(' '))
+      combination.shift()
+      //console.log(combination.join(' '))
+      combination.reverse()
+      combinationsNot.push('0 ' + combination.join(' ')) //o caminho de ida ou volta é o mesmo
+      //console.log(combination.join(' '))
+
+      i = i + 2
     }
   }
   console.log('combinations done')
+  //console.log(combinations)
+  //console.log(combinationsNot)
   return combinations
 }
 
